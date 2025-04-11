18ce987460dbd031cf6f4022ce0ddcb06c0518b6
Prathamesh <114254813+prathameshbhangale@users.noreply.github.com>
2025-04-11 22:33:48 +0530

    jwt authentication

--- src/main/java/com/urlShortner/shortner/servise/UserDetailsImpl.java
+++ src/main/java/com/urlShortner/shortner/servise/UserDetailsImpl.java
@@ -1,24 +1,59 @@
 package com.urlShortner.shortner.servise;
 
+import com.urlShortner.shortner.models.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+@Data
 public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Collection<? extends GrantedAuthority> authorities;
+    private String username;
+    private String password;
+    private String email;
+    private Long id;
+
+    public UserDetailsImpl(
+            Long id,String username,String password, String email,Collection<? extends GrantedAuthority> authorities
+    ){
+        this.id = id;
+        this.username = username;
+        this.password=password;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(authority)
+        );
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return username;
     }
 }
--- src/main/java/com/urlShortner/shortner/servise/UserDetailsServiceImpl.java
+++ src/main/java/com/urlShortner/shortner/servise/UserDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package com.urlShortner.shortner.servise;
 
-public class UserDetailsServiceImpl {
+import com.urlShortner.shortner.models.User;
+import com.urlShortner.shortner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: "+ username));
+        return UserDetailsImpl.build(user);
+    }
 }
