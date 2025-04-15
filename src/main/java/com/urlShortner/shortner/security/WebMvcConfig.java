38a9e5595be43e9bb6d17c89b4fe6d1e5f570ae5
Prathamesh <114254813+prathameshbhangale@users.noreply.github.com>
2025-04-15 16:57:36 +0530

    cors

---
+++ src/main/java/com/urlShortner/shortner/security/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.urlShortner.shortner.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins(frontendUrl) // Allow the frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
